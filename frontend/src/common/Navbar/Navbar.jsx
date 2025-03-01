c20e64b71a4d1c83c4b22253e2c74303151957ec
Ben Getches <benjamin.getches@colorado.edu>
2025-03-01 16:49:10 -0700

    dummy log in

--- frontend/src/common/Navbar/Navbar.jsx
+++ frontend/src/common/Navbar/Navbar.jsx
@@ -1,8 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
-const Navbar = ({ isLoggedIn }) => {
+const Navbar = ({ isLoggedIn, onLogout }) => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+  
+  useEffect(() => {
+    // Get user info from localStorage if logged in
+    if (isLoggedIn) {
+      try {
+        const userInfo = JSON.parse(localStorage.getItem("user"));
+        if (userInfo && userInfo.name) {
+          setUserName(userInfo.name);
+        } else if (userInfo && userInfo.email) {
+          // Use email as fallback
+          setUserName(userInfo.email.split("@")[0]);
+        }
+      } catch (error) {
+        console.error("Error parsing user info:", error);
+      }
+    }
+  }, [isLoggedIn]);
+  
+  const handleLogout = () => {
+    if (onLogout) {
+      onLogout();
+      navigate("/");
+    }
+  };
+
+  const handleHomeClick = (e) => {
+    e.preventDefault();
+    navigate('/home', { state: { fromLogin: true } });
+  };
+  
+  const handleSignIn = () => {
+    navigate('/signin');
+  };
+  
+  const handleSignUp = () => {
+    navigate('/signup');
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
@@ -11,10 +51,11 @@ const Navbar = ({ isLoggedIn }) => {
       <div className="navbar-links">
         {isLoggedIn ? (
           <>
-            <Link to="/home">Dashboard</Link>
+            <Link to="/home" onClick={handleHomeClick}>Dashboard</Link>
             <Link to="/recipes">Recipes</Link>
             <Link to="/profile">Profile</Link>
-            <button className="logout-button">Logout</button>
+            {userName && <span className="user-greeting">Hello, {userName}</span>}
+            <button className="logout-button" onClick={handleLogout}>Logout</button>
           </>
         ) : (
           <>
@@ -26,8 +67,8 @@ const Navbar = ({ isLoggedIn }) => {
             <Link to="/dev-login" className="dev-link">
               Dev: Auto Login
             </Link>
-            <button className="login-button">Login</button>
-            <button className="signup-button">Sign Up</button>
+            <button className="login-button" onClick={handleSignIn}>Login</button>
+            <button className="signup-button" onClick={handleSignUp}>Sign Up</button>
           </>
         )}
       </div>
